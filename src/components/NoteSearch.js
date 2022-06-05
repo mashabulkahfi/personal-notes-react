9367a62de6c2ce09e33f3a283d9267c85f60f13e
Muhammad Ashabul Kahfi <mashabulkahfi8@gmail.com>
2022-06-05 19:49:52 +0700

    change NoteSearch from Class Component to Function Component

--- src/components/NoteSearch.js
+++ src/components/NoteSearch.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
-class NoteSearch extends React.Component {
-	constructor(props) {
-		super(props);
-
-		//inisialisasi state
-		this.state = {
-			keywordSearch: '',
-		}
-		this.onKeywordChangeEventHandler = this.onKeywordChangeEventHandler.bind(this);
+function NoteSearch({ keywordSearch, searchKeyword }){
+	function onKeywordChangeEventHandler(event){
+		searchKeyword({ keywordSearch: event.target.value });
 	}
 
-	onKeywordChangeEventHandler(event) {
-		this.setState({
-			keywordSearch: event.target.value,
-		});
-		this.props.searchKeyword({ keywordSearch: event.target.value.toLowerCase() });
-	}
-
-	render() {
-		return (
-			<input type="text" placeholder="Cari catatan ..." value={this.state.keywordSearch} onChange={this.onKeywordChangeEventHandler}></input>
-		);
-	}
+	return (
+		<input type="text" placeholder="Cari catatan ..." value={keywordSearch} onChange={onKeywordChangeEventHandler}></input>
+	);
 }
+
+// class NoteSearch extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.onKeywordChangeEventHandler = this.onKeywordChangeEventHandler.bind(this);
+// 	}
+
+// 	onKeywordChangeEventHandler(event) {
+// 		// this.setState({
+// 		// 	keywordSearch: event.target.value,
+// 		// });
+// 		this.props.searchKeyword({ keywordSearch: event.target.value.toLowerCase() });
+// 	}
+
+// 	render() {
+// 		return (
+// 			<input type="text" placeholder="Cari catatan ..." value={this.props.keywordSearch} onChange={this.onKeywordChangeEventHandler}></input>
+// 		);
+// 	}
+// }
 
 export default NoteSearch;
